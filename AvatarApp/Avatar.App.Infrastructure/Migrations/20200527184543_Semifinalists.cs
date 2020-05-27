1a3f2ad5c0a694a0cb8e4c51773416b28736f964
Арендарский Данил <arendarskydd@gmail.com>
2020-05-27 21:49:51 +0300

    fix db prod

---
+++ AvatarApp/Avatar.App.Infrastructure/Migrations/20200527184543_Semifinalists.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Avatar.App.Infrastructure.Migrations
+{
+    public partial class Semifinalists : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "VideoId",
+                table: "WatchedVideos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserId",
+                table: "WatchedVideos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "WatchedVideos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserId",
+                table: "Videos",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Videos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            //migrationBuilder.AddColumn<string>(
+            //    name: "FireBaseId",
+            //    table: "Users",
+            //    nullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ToId",
+                table: "Messages",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "FromId",
+                table: "Messages",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Messages",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "VideoId",
+                table: "LikedVideos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserId",
+                table: "LikedVideos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "LikedVideos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateTable(
+                name: "Semifinalists",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Semifinalists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Semifinalists_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Semifinalists_UserId",
+                table: "Semifinalists",
+                column: "UserId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Semifinalists");
+
+            migrationBuilder.DropColumn(
+                name: "FireBaseId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VideoId",
+                table: "WatchedVideos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "WatchedVideos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "WatchedVideos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Videos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Videos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ToId",
+                table: "Messages",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FromId",
+                table: "Messages",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Messages",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VideoId",
+                table: "LikedVideos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "LikedVideos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "LikedVideos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
